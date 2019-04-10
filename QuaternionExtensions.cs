905450d8f7667f89d7bed522697f79a238e42d5d
Mikhail Naumov <mikhail_naumov@yahoo.com>
2019-04-09 23:56:08 -0700

    Added more extensions

---
+++ QuaternionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace cpGames.core
+{
+    public static class QuaternionExtensions
+    {
+        #region Methods
+        public static Quaternion FromFloat(float[] f)
+        {
+            if (f == null || f.Length < 4)
+            {
+                return Quaternion.identity;
+            }
+            return new Quaternion(f[0], f[1], f[2], f[3]);
+        }
+
+        public static void ToFloat(this Quaternion q, ref float[] result)
+        {
+            if (result == null)
+            {
+                result = new float[4];
+            }
+
+            result[0] = q.x;
+            result[1] = q.y;
+            result[2] = q.z;
+            result[3] = q.w;
+        }
+
+        public static float[] ToFloat(this Quaternion q)
+        {
+            var result = new float[4];
+
+            result[0] = q.x;
+            result[1] = q.y;
+            result[2] = q.z;
+            result[3] = q.w;
+
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ Vector3Extensions.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace cpGames.core
+{
+    public static class Vector3Extensions
+    {
+        #region Methods
+        public static Vector3 FromFloat(float[] f)
+        {
+            if (f == null || f.Length < 3)
+            {
+                return Vector3.zero;
+            }
+            return new Vector3(f[0], f[1], f[2]);
+        }
+
+        public static void ToFloat(this Vector3 v, ref float[] result)
+        {
+            if (result == null)
+            {
+                result = new float[3];
+            }
+
+            result[0] = v.x;
+            result[1] = v.y;
+            result[2] = v.z;
+        }
+
+        public static float[] ToFloat(this Vector3 v)
+        {
+            var result = new float[3];
+
+            result[0] = v.x;
+            result[1] = v.y;
+            result[2] = v.z;
+
+            return result;
+        }
+        #endregion
+    }
+}
